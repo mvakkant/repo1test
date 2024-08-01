b6ccf7c04051b67cdca362597578178ec4a07e65
mounika vakkanti <mounikavakkanti1997@gmail.com>
2024-07-31 22:05:46 -0500

    adding new file

---
+++ code_1.py
@@ -0,0 +1,16 @@
+# import cv2
+# import matplotlib.pyplot as plt
+# import skimage import measure, morphology
+# import skimage.color import label2rgb
+# import skimage.measure
+# import numpy as np
+# from qtconsole.mainwindow import background
+
+constant_parameter_1 = 84
+constant_parameter_2 = 250
+constant_parameter_3 = 100
+constant_parameter_4 = 18
+# blobs_labels = measure.tk.Label(blobs, background=1)
+# image_label_overlay = label2rgb(blobs_labels, image = img)
+
+
